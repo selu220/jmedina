c1966f749db4238e42e5c1160bff1ffa08af2c24
Jose Medina <metroselu@gmail.com>
2020-12-30 09:23:27 -0500

    Updated function to address assignment. Ran several tests - pyramids of size 3, 5, 10, 20, etc. All render appropriately in terminal.

--- pyramid.py
+++ pyramid.py
@@ -1,11 +1,23 @@
-#!/usr/bin/env python3
+
 """Print a pyramid to the terminal
+
+get get expected final width
 
 A pyramid of height 3 would look like:
 
 --=--
 -===-
 =====
+
+
+my pseudo-code to decode pattern
+
+---=--- f(row 1, height = 4 ) = 7 across, 7/2 = 3.5
+--===-- f(row 2, height = 4 ) = 7 across, with 3 equal ( 2 * 3 = 6 - 1 = 5 , etc.
+-=====-  +2 equals, -1 dashes given the dash_factor
+=======
+
+
 
 """
 
@@ -17,14 +29,25 @@ def print_pyramid(rows):
 
     :param int rows: total height
     """
-    raise NotImplementedError("Called with rows={}".format(rows))
+    # set starting values which we'll increment / decrement according to pattern
+    dash_factor = ((rows * 2) - 1) / 2
+    dash_count = int(dash_factor)
+    equal_count = 1
+
+    for i in range(0, rows):
+        d = dash_count * "-"
+        e = equal_count * "="
+        print(d + e + d)
+
+        dash_count -= 1
+        equal_count += 2
 
 
 if __name__ == "__main__":
     parser = ArgumentParser(
         description=__doc__, formatter_class=RawDescriptionHelpFormatter
     )
-    parser.add_argument("-r", "--rows", default=10, help="Number of rows")
+    parser.add_argument("-r", "--rows", default=3, help="Number of rows")
 
     args = parser.parse_args()
     print_pyramid(args.rows)
